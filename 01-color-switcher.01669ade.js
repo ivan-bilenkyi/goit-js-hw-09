1088ec870677064af5d4f0722e153ae24cdba768
ivan-bilenkyi <128532966+ivan-bilenkyi@users.noreply.github.com>
2023-09-18 18:31:31 +0000

    Deploying to gh-pages from @ ivan-bilenkyi/goit-js-hw-09@7bcbbd0a7a4f5c5da49613386ee1bcfc49d875b4 🚀

---
+++ 01-color-switcher.01669ade.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),a=document.querySelector("body"),n=null;e.disabled=!0,t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,n=setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0));a.style.backgroundColor=t}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(n)}))}();
+//# sourceMappingURL=01-color-switcher.01669ade.js.map
